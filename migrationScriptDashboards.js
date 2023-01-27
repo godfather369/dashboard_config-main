02423216128c8741d67f485f13903c68e8d3d976
CharanMandapati <charan.mandapati@regology.com>
2023-01-27 14:33:47 +0530

    Updating Migration Script

--- migrationScriptDashboards.js
+++ migrationScriptDashboards.js
@@ -1,34 +1,59 @@
-var fs = require('fs');
+var fs = require("fs");
 
-const dashboardsCollection = require('./reg_dashboards');
-const widgetsCollection = require('./reg_dashboard_widgets.json');
-const { myTenantId } = require('./constants');
+const dashboardsCollection = require("./reg_dashboards");
+const widgetsCollection = require("./reg_dashboard_widgets.json");
+const filtersCollection = require("./reg_dashboard_filters");
+const { myTenantId } = require("./constants");
 
-const {ObjectId} = require('mongodb'); 
+const { ObjectId } = require("mongodb");
 
 let updateddashboardsCollection = dashboardsCollection.map((item) => {
 	const myId = new ObjectId();
 	let widgets = item["widgets"];
 	widgets.forEach((widget) => {
-		for(let i = 0 ; i < widgetsCollection.length ; i++){
-			if(widget["widgetId"] === widgetsCollection[i]["id"]){
+		for (let i = 0; i < widgetsCollection.length; i++) {
+			if (widget["widgetId"] === widgetsCollection[i]["id"]) {
 				widget["widgetId"] = widgetsCollection[i]["_id"];
 			}
 		}
-	})
+		let dropDownFilters = widget["dropDownFilters"];
+		if (dropDownFilters) {
+			dropDownFilters.forEach((dropDownFilter) => {
+				for (let j = 0; j < filtersCollection.length; j++) {
+					debugger;
+					if (dropDownFilter["filter"] === filtersCollection[j]["id"]) {
+						dropDownFilter["filter"] = filtersCollection[j]["_id"];
+					}
+				}
+			});
+		}
+	});
+
 	const widgetsItem = widgets.map((item) => {
 		return {
-			"widget" : item.widgetId,
-			"layout" : item.layout,
-			"viewType" : item.viewType
-		}
-	})
-	const updatedItem = {...item , "widgets" : widgetsItem  , "tenantId" : myTenantId ,  "_id" : {"$oid": myId}}
+			widget: item.widgetId,
+			layout: item.layout,
+			viewType: item.viewType,
+			dropDownFilters: item.dropDownFilters,
+		};
+	});
+
+	console.log(widgetsItem);
+	const updatedItem = {
+		...item,
+		widgets: widgetsItem,
+		tenantId: myTenantId,
+		_id: { $oid: myId },
+	};
 	return updatedItem;
-})
-
-fs.unlinkSync('reg_dashboards.json');
-
-fs.writeFile("reg_dashboards.json",JSON.stringify(updateddashboardsCollection),function(err, result) {
-    if(err) console.log('error', err);
 });
+
+fs.unlinkSync("reg_dashboards.json");
+
+fs.writeFile(
+	"reg_dashboards.json",
+	JSON.stringify(updateddashboardsCollection),
+	function (err, result) {
+		if (err) console.log("error", err);
+	}
+);
