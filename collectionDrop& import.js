13e3d7f0a0a0f798eebed5b4be272a4b66ca8cbb
CharanMandapati <charan.mandapati@regology.com>
2023-08-21 11:37:16 +0530

    dev sync

--- collectionDrop& import.js
+++ collectionDrop& import.js
@@ -1,12 +1,12 @@
 const exec = require("child_process").exec;
-exec("mongo RegHub --eval 'db.tasks.drop()'", (e) => {
+exec("mongo RegHub --eval 'db.reg_map_generic.drop()'", (e) => {
 	if (e instanceof Error) {
 		console.error(e);
 		throw e;
 	}
 });
 exec(
-	"mongoimport --uri mongodb://localhost:27017/ --db RegHub --collection tasks  tasks.json",
+	"mongoimport --uri mongodb://localhost:27017/ --db RegHub --collection reg_map_generic  reg_map_generic.json",
 	(e) => {
 		if (e instanceof Error) {
 			console.error(e);
